dbf3180e3df7ea6cf5cce5b77b8d6441063469be
Petermuturi <pmuturi65@gmail.com>
2017-03-11 10:15:39 +0300

    added item handler

--- App.js
+++ App.js
@@ -5,18 +5,49 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
+  Platform
 } from 'react-native';
 
+import Header from './components/structure/Header';
+import Footer from './components/structure/Footer';
+
 class App extends Component {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	value: '',
+	  	items: []
+	  };
+	}
+	handleAddItem(){
+		if(!this.state.value) return;
+		
+	}
   render() {
     return (
-      <View />
+      <View style={styles.container}>
+      	<Header/>
+      	<View style={styles.content}>
+      		
+      	</View>
+      	<Footer/>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-
+	container: {
+		flex: 1,
+		backgroundColor: '#F5F5F5',
+		...Platform.select({
+			ios: { paddingTop: 30 }
+		})
+	},
+	content: {
+		flex: 1
+	}
 });
 
 
--- components/structure/Header.js
+++ components/structure/Header.js
@@ -5,18 +5,31 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
+  TextInput
 } from 'react-native';
 
 class Header extends Component {
   render() {
     return (
-      <View />
+      <View>
+      <TextInput
+				placeholder="What's on your mind?"
+				blurOnSubmit={false}
+				returnKeyType='add'
+				style={styles.input}
+      />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-
+	header: {
+		paddingHorizontal: 16,
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		alignItems: 'center'
+	}
 });
 
 
